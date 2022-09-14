47ae53e49beff83a58cd70a1a534dc1c0f90d39f
강혁준 <gurwns232@naver.com>
2022-09-15 04:04:20 +0900

    initTimer func

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const wordText = document.querySelector('.word'),
     hintText = document.querySelector('.hint span'),
+    timeText = document.querySelector('.time b'),
     inputField = document.querySelector('input'),
     refreshBtn = document.querySelector('.refresh-word'),
     checkBtn = document.querySelector('.check-word')
 
-let correctWord
+let correctWord, timer
+
+const initTimer = maxTime => {
+    clearInterval(timer)
+    timer = setInterval(() => {
+        if (maxTime > 0) {
+            maxTime--
+            return (timeText.innerText = maxTime)
+        }
+        clearInterval(timer)
+        alert(`Time off! ${correctWord.toUpperCase()} was the correct word`)
+        initGame()
+    }, 1000)
+}
 
 const initGame = () => {
+    initTimer(30)
     let randomObj = words[Math.floor(Math.random() * words.length)]
     let wordArray = randomObj.word.split('')
 
