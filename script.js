7a619d75710dcd3adc0029ce699973a703e755e7
강혁준 <gurwns232@naver.com>
2022-09-15 03:30:46 +0900

    word scramble init func

---
+++ script.js
@@ -0,0 +1,23 @@
+const wordText = document.querySelector('.word'),
+    hintText = document.querySelector('.hint span'),
+    refreshBtn = document.querySelector('.refresh-word')
+
+const initGame = () => {
+    let randomObj = words[Math.floor(Math.random() * words.length)]
+    let wordArray = randomObj.word.split('')
+
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1))
+
+        // let temp = wordArray[i]
+        // wordArray[i] = wordArray[j]
+        // wordArray[j] = temp
+
+        ;[wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]]
+    }
+    wordText.innerText = wordArray.join('')
+    hintText.innerText = randomObj.hint
+    console.log(randomObj)
+}
+
+initGame()
